d54da3e0875d2b761b59c9b5eba69f5dee47a9c7
ShyamsaranRajendran <sarancode6@gmail.com>
2025-01-02 21:01:10 +0530

    jan 221:01

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import Hero from './components/Hero.jsx';
 import Projects from './components/Projects.jsx';
 import Skills from './components/Skills.jsx';
 import Contact from './components/Contact.jsx';
-import './styles.css';
 function App() {
   return (
     <div className="bg-gray-900">
